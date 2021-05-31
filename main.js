49c6d646a01c9aaa42ba55e00f50908a8c98528e
CarelTransformIt <84836107+CarelTransformIt@users.noreply.github.com>
2021-05-31 16:45:58 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,33 @@ document.addEventListener("orientationchange", function (event) {
   }
 });
 
-document.addEventListener("load", function (event) {
-  console.log("Page loaded");
+
+window.addEventListener('load', (event) => {
+  var orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
+
+  if (orientation === "landscape-primary") {
+    console.log("That looks good.");
+  } else if (orientation === "landscape-secondary") {
+    console.log("Mmmh... the screen is upside down!");
+  } else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+    console.log("Mmmh... you should rotate your device to landscape");
+  } else if (orientation === undefined) {
+    console.log("The orientation API isn't supported in this browser :(");
+  }
+
+  start();
+
 });
+
+
+function ready() {
+  const { type } = screen.orientation;
+  console.log(`Fullscreen and locked to ${type}. Ready!`);
+}
+
+async function start() {
+  console.log("Start");
+  await document.body.requestFullscreen();
+  await screen.orientation.lock("landscape");
+  ready();
+}
