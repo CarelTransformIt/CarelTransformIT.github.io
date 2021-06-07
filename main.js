2698af6355e9a23336c336c830f48f4014a33bd6
CarelTransformIt <84836107+CarelTransformIt@users.noreply.github.com>
2021-06-07 12:05:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+//To check for horizontal
+document.addEventListener("orientationchange", function (event) {
+  switch (window.orientation) {
+    case -90:
+    case 90:
+      console.log("90 degrees");
+      break;
+    default:
+      console.log("Default");
+  }
+});
+
+
+window.addEventListener('load', (event) => {
+  var orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
+
+  if (orientation === "landscape-primary") {
+    console.log("That looks good.");
+  } else if (orientation === "landscape-secondary") {
+    console.log("Mmmh... the screen is upside down!");
+  } else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+    console.log("Mmmh... you should rotate your device to landscape");
+  } else if (orientation === undefined) {
+    console.log("The orientation API isn't supported in this browser :(");
+  }
+
+});
